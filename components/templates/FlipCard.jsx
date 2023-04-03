ad5c72dbc98cdb8e0afee4e1daada9bba3c72ea0
Anjelica Wright <55635024+anjelicajwright@users.noreply.github.com>
2023-04-03 20:21:11 +0200

    fix build

--- components/templates/FlipCard.jsx
+++ components/templates/FlipCard.jsx
@@ -3,39 +3,36 @@ import { useState } from "react";
 
 export function FlipCard({ questions }) {
   const [styling, setStyling] = useState(false);
-
-  if (window.screen.width > 700) {
-    return (
-      <>
-        <div className={style.cardCont}>
-          {questions.map((item) => (
-            <div className={style.flipCard}>
+  return (
+    <>
+      <div className={style.cardCont}>
+        {questions.map((item) => (
+          <div className={style.flipCard}>
+            <div
+              onClick={() => {
+                setStyling(styling === item.question ? false : item.question);
+              }}
+              className={
+                styling === item.question ? style.rotate : style.wrapper
+              }
+            >
               <div
-                onClick={() => {
-                  setStyling(styling === item.question ? false : item.question);
-                }}
                 className={
-                  styling === item.question ? style.rotate : style.wrapper
+                  styling === item.question
+                    ? style.flipCardBack
+                    : style.flipCardFront
                 }
               >
-                <div
-                  className={
-                    styling === item.question
-                      ? style.flipCardBack
-                      : style.flipCardFront
-                  }
-                >
-                  {styling === item.question ? (
-                    <p>{item.answer}</p>
-                  ) : (
-                    <h2>{item.question}</h2>
-                  )}
-                </div>
+                {styling === item.question ? (
+                  <p>{item.answer}</p>
+                ) : (
+                  <h2>{item.question}</h2>
+                )}
               </div>
             </div>
-          ))}
-        </div>
-      </>
-    );
-  }
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
