1792532276519cf5495e7c63e0e8932881e60591
jjelljelly <anjelicajwright@hotmail.com>
2024-07-09 13:30:40 +0200

    update video

--- components/home/Video.jsx
+++ components/home/Video.jsx
@@ -1,7 +1,7 @@
 export function Video() {
     return (
         <video width="100%" height="420" controls >
-            <source src="https://beta.londonfootandanklesurgery.co.uk/wp-content/uploads/2024/05/LFAS-Video.mp4" type="video/mp4" />
+            <source src="https://www.londonfootandanklesurgery.co.uk/wp-content/uploads/2024/05/LFAS-Video.mp4" type="video/mp4" />
             Your browser does not support the video tag.
         </video>
     )
